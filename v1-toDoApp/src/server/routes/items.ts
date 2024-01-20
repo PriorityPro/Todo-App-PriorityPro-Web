1444daaec5ec1aa6cda6eefb2c658a6840403557
Matt Mattox <bizofmatt@gmail.com>
2024-01-20 15:18:48 -0800

    created server file and some basic routes

---
+++ v1-toDoApp/src/server/routes/items.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const router = express.Router();
+// instantize an app from express() function
+
+//get the items for our todo list
+//router.get("/items") === router.get("/")
+router.get("/", (_req: Request, res: Response) => {
+  res.send("sending the todo list items")
+})
+
+//post to create our items for a todo list
+router.post('/', (_req: Request, res: Response) => {
+res.send('create a todo item')
+})
+
+//access an individual item
+router.get('/:id', (_req: Request, res: Response) => {
+_req.params.id
+res.send(`get an individual item ${_req.params.id}`)
+  })
+
+export default router;
--- v1-toDoApp/src/server/server.ts
+++ v1-toDoApp/src/server/server.ts
@@ -3,19 +3,31 @@
 // to understand information, he's a psycho information broker after all
 // ofc we will use ES module system 
 import express, { Request, Response } from "express";
+import itemRouter from './routes/items.ts';
 
 // instantize an app from express() function
 const app = express();
 
 // setup a simple get route 
 // and return a test message
-
 app.get("/", (_req: Request, res: Response) => {
   res.json({ test: "Ok" });
 });
 
+app.get("/items", (_req: Request, res: Response) => {
+  res.send("sending the todo list items")
+})
+
+
+
+//router for items
+app.use('/items', itemRouter);
+//create our post router
+
+
+
 // and finally start the server at port 5000
-const PORT = 5005;
+const PORT = 5008;
 app.listen(PORT, () => {
   console.log("server has started on port");
   console.log("http://localhost:" + PORT);
