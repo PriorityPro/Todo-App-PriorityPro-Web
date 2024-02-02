de2767672c6b13ac70244f24f804b453a3ab5e02
Matt Mattox <bizofmatt@gmail.com>
2024-02-01 17:45:10 -0800

    touches towards getting backend to frontend

--- v1-toDoApp/src/client/components/FetchItems.tsx
+++ v1-toDoApp/src/client/components/FetchItems.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import api from '../api/ItemFinder.ts'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 
