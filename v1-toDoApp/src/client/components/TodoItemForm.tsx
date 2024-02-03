d8ba9c97df0a13b969a7137d20c50555672f602f
Matt Mattox <bizofmatt@gmail.com>
2024-02-03 15:46:53 -0800

    Fixed create method in our itemsController to handle users only adding a title (todo item) leaving the completed field false as well adding the newItem to our res.locals

--- v1-toDoApp/src/client/components/TodoItemForm.tsx
+++ v1-toDoApp/src/client/components/TodoItemForm.tsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
 import api from '../api/ItemCreator.ts';
 
-
+interface Todo {
+  title: string,
+  complete: boolean
+}
 
 
 const TodoItemForm = () => {
   const [title, setTitle] = useState('');
   const [complete, setComplete] = useState(false);
+  const [todos, setTodos] = useState<Todo[]>([]); // Explicitly define the type
+
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.id === 'title') {
+      setTitle(event.target.value);
+    } else if (event.target.id === 'completed') {
+      setComplete(event.target.checked);
+    }
+  };
 
   const addItemToDatabase = async () => {
     if (!title) {
@@ -18,37 +32,32 @@ const TodoItemForm = () => {
       title: title,
       completed: complete,
     };
-
+    
     try {
-      const response = await api.post('http://localhost:5008/api/v1/items/create', requestBody, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
+      const response = await api.post('http://localhost:5008/api/v1/items/create', requestBody);
       console.log('API response', response);
     } catch (error) {
       console.error('Could not add new item to the list', error.response.data);
     }
+    setTodos([...todos, { title, complete }]);
   };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.id === 'title') {
-      setTitle(event.target.value);
-    } else if (event.target.id === 'completed') {
-      setComplete(event.target.checked);
-    }
-  };
-
+  
+ 
+//on form submission we don't want to reload the page
+//invoking our addItemToDatabase function which is making the request
+//as well as adding our items to our array
+//updating the state for the title to an empty string
+//updating the state to false
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    window.location.reload();
     addItemToDatabase();
+    setTitle('')
+    setComplete(false);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <div id="form">
         <label htmlFor="title">Todo item:</label>
         <br />
         <input type="text" id="title" placeholder="" value={title} onChange={handleChange} required />
@@ -60,6 +69,15 @@ const TodoItemForm = () => {
         <input type="checkbox" id="completed" checked={complete} onChange={handleChange} />
       </div>
       <input type="submit" value="Submit" />
+
+      <div>
+        <h2>Todo List:</h2>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo.title} - {todo.complete ? 'Completed' : 'Incomplete'}</li>
+          ))}
+        </ul>
+      </div>
     </form>
   );
 };
