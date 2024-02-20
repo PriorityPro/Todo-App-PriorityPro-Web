5d694ee88dbfff0ee2c209fd560c7de9003dacc1
Matt Mattox <bizofmatt@gmail.com>
2024-02-19 20:50:20 -0800

    implemented react router

---
+++ v1-toDoApp/src/client/components/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div>
+      <h2>This is our Calendar Page</h2>
+    </div>
+  )
+}
+
+export default Calendar
--- v1-toDoApp/src/main.tsx
+++ v1-toDoApp/src/main.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.tsx'
+import Calendar from './client/components/Calendar.tsx';
 import './globals.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   //Its worth noting that in React 18 when you run in dev mode with React.StrictMode on. Your useEffect hook will always run atleast twice because your component is mounted twice.
   <BrowserRouter>
-  <Routes>
-  <Route path="/" element={<App />} />
-  </Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/calendar" element={<Calendar />} />
+    </Routes>
 </BrowserRouter>,
 )
